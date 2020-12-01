f08ce7e19e456618992bd200267ba4382776c71a
Vojtěch Šanda <vojtech.sanda@gmail.com>
2020-12-01 16:53:25 +0100

    Template for a day

---
+++ template.js
@@ -0,0 +1,26 @@
+/**
+ * PART 1 TEXT
+ * 
+ * --- Part Two ---
+ * PART 2 TEXT
+ */
+
+const fs = require('file-system');
+const path = require('path')
+
+const rawdata = fs.readFileSync(path.resolve(__dirname, 'input.json'));
+const input = JSON.parse(rawdata);
+
+/*------------*/
+/*-- Part 1 --*/
+/*------------*/
+// PART 1 CODE
+
+console.log(`Part 1: ${'PART_1_VAR'}`); // Should be: PART 1 Solution
+
+/*------------*/
+/*-- Part 2 --*/
+/*------------*/
+// PART 2 CODE
+
+console.log(`Part 1: ${'PART_1_VAR'}`); // Should be: PART 2 Solution
