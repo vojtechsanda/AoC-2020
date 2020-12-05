9c30dd85c917344c289f92135d1bee39181a84d1
Vojtěch Šanda <vojtech.sanda@gmail.com>
2020-12-05 01:29:51 +0100

    Refactored day 4

--- day_04/app.js
+++ day_04/app.js
@@ -12,119 +12,100 @@ const rawData = fs.readFileSync(dataPath);
 /*----------------------*/
 const rows = rawData.toString().split('\r\n');
 
+const passportRows = [];
+let passportIndex = 0;
+
+for (const row of rows) {
+    if (row === '') {
+        passportIndex++;
+        continue;
+    }
+    if (!passportRows[passportIndex]) {
+        passportRows[passportIndex] = [];
+    }
+    
+    passportRows[passportIndex].push(row);
+};
+
+const passportStrings = passportRows.map(row => row.join(' '));
+
+const passports = passportStrings.map(passportString => {
+    const items = passportString.split(' ').map(item => item.split(':'));
+
+    const passportObj = {};
+    
+    items.forEach(item => {
+        passportObj[item[0]] = item[1];
+    })
+
+    return passportObj;
+})
+
 /*------------*/
 /*-- Part 1 --*/
 /*------------*/
-let x = 0
-let d = []
+const solvePart1 = (input, consoleOutput = true) => {
+    const requiredFields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
 
-rows.forEach(r => {
-    if (!d[x]) {
-        d[x] = []
-    }
+    const validPassports = input.filter(passport => {
+        const missingFields = requiredFields.filter(requiredField => !passport[requiredField]);
 
-    if (r === '') {
-        x++
-    } else {
-        d[x].push(r)
-    }
-})
-
-q = d.map(e => e.join(' '))
-
-r = q.map(d => d.split(' '))
-
-c = r.map(s => s.map(b => b.split(':')))
-
-required = [
-    'byr',
-    'iyr',
-    'eyr',
-    'hgt',
-    'hcl',
-    'ecl',
-    'pid',
-]
-
-h = c.filter(arr => {
-    arr = arr.map(p => p[0])
-    let x = required.filter(fil => {
-        if (arr.indexOf(fil) >= 0) {
-            return false
-        }
-
-        return true
+        return missingFields.length === 0;
     })
 
-    if (x.length > 0) {
-        return false
+    if (consoleOutput) {
+        console.log(`The answer to the part 1 is => ${validPassports.length}`); // Should be: 192
     }
 
-    return true
-})
-
-g = h.filter(q => {
-    k = {}
-    o = q.forEach(w => {
-        k[w[0]] = w[1]
-    })
-
-    if (Number(k.byr) >= 1920 && Number(k.byr) <= 2002) {
-        
-        if (Number(k.iyr) >= 2010 && Number(k.iyr) <= 2020) {
-            
-            if (Number(k.eyr) >= 2020 && Number(k.eyr) <= 2030) {
-                
-                if (['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].indexOf(k.ecl) >= 0) {
-                    
-                    if (!isNaN(k.pid) && k.pid.length === 9) {
-                        
-                        let hcl = k.hcl.split('')
-                        let hash = hcl.splice(0, 1).join('')
-
-                        if (hash === '#') {
-                            
-                            let allowed = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
-
-                            x = hcl.filter(j => allowed.indexOf(j) >= 0)
-
-                            if (x.length === hcl.length) {
-                                
-                                hgt = k.hgt.split('')
-
-                                unit = hgt.splice((hgt.length - 2)).join('')
-                                num = Number(hgt.join(''))
-
-                                if (unit === 'cm' && num >= 150 && num <= 193) {
-                                    
-                                    return true
-                                } else if (unit === 'in' && num >= 59 && num <= 76) {
-                                    
-                                    return true
-                                }
-
-                                return false
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-})
-console.log(g.length);
-
-
-// (() => {
-//     // PART 1 CODE HERE
-//     console.log(`Part 1: ${'PART_1_VAR'}`); // Should be: PART 1 Solution
-// })()
+    return [validPassports.length, validPassports];
+}
+solvePart1(passports);
 
 
 /*------------*/
 /*-- Part 2 --*/
 /*------------*/
-// (() => {
-//     // PART 2 CODE HERE
-//     console.log(`Part 2: ${'PART_2_VAR'}`); // Should be: PART 2 Solution
-// })()
+const solvePart2 = (input, consoleOutput = true) => {
+    const prevalidatedInput = solvePart1(input, false)[1];
+
+    const fullyValidPassports = prevalidatedInput.filter(passport => {
+        const validators = {
+            cid: cid => true,
+            byr: byr => Number(byr) >= 1920 && Number(byr) <= 2002,
+            iyr: iyr => Number(iyr) >= 2010 && Number(iyr) <= 2020,
+            eyr: eyr => Number(eyr) >= 2020 && Number(eyr) <= 2030,
+            ecl: ecl => ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].indexOf(ecl) > -1,
+            pid: pid => !isNaN(pid) && pid.length === 9,
+            hcl: hcl => {
+                const hexVals = '0123456789abcdef';
+                const invalidVals = hcl.slice(1).split('').filter(char => hexVals.indexOf(char) === -1);
+
+                return hcl[0] === '#' && invalidVals.length === 0;
+            },
+            hgt: hgt => {
+                const unit = hgt.slice(-2);
+                const val = Number(hgt.slice(0, hgt.length-2));
+
+                if (
+                    unit === 'cm' && val >= 150 && val <= 193 ||
+                    unit === 'in' && val >= 59 && val <= 76
+                ) {
+                    return true;
+                }
+
+                return false;
+            }
+        };
+
+        const invalidItems = Object.keys(passport).filter(item => !validators[item](passport[item]));
+
+        return invalidItems.length === 0;
+    })
+
+    if (consoleOutput) {
+        console.log(`The answer to the part 2 is => ${fullyValidPassports.length}`); // Should be: 101
+    }
+    
+    return [fullyValidPassports.length, fullyValidPassports];
+}
+solvePart2(passports);
