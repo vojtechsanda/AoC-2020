96ac18fecd0a633c7ff9e405a45ce6a54f2cbe77
Vojtěch Šanda <vojtech.sanda@gmail.com>
2020-12-10 23:04:41 +0100

    Day 10, Part 2

--- day_10/app.js
+++ day_10/app.js
@@ -22,9 +22,9 @@ numRows.sort((a,b) => a-b);
 const solvePart1 = (input, consoleOutput = true) => {
     let sum = [];
 
-    for (let i = 1; i < numRows.length; i++) {
-        const item = numRows[i];
-        const prevItem = numRows[i-1];
+    for (let i = 1; i < input.length; i++) {
+        const item = input[i];
+        const prevItem = input[i-1];
 
         const diff = item - prevItem;
 
@@ -37,24 +37,63 @@ const solvePart1 = (input, consoleOutput = true) => {
     const finalNum = sum[0] * sum[2];
 
     if (consoleOutput) {
-        console.log(`The answer to the part 1 is => ${finalNum}`); // Should be: 2240
+        console.log(`The answer to the part 1 is => ${finalNum}`); // Example: 220, Normal: 2240
     }
 
     return [finalNum];
 }
-solvePart1('INPUT');
+solvePart1(numRows);
 
 
 /*------------*/
 /*-- Part 2 --*/
 /*------------*/
 const solvePart2 = (input, consoleOutput = true) => {
-    // PART 2 CODE HERE
+    const editableInput = [0, ...Array.from(input)];
 
-    if (consoleOutput) {
-        console.log(`The answer to the part 2 is => ${'PART_2_SOLUTION'}`); // Should be: PART_2_SOLUTION
+    let blocks = [];
+    let lastSplit = 0;
+
+    for (let index = 1; index < editableInput.length; index++) {
+        const item = editableInput[index];
+        const prevItem = editableInput[index-1];
+
+        if (item - prevItem === 3) {
+            blocks = [...blocks, editableInput.slice(lastSplit, index)];
+            lastSplit = index;
+        } else if (index === editableInput.length - 1) {
+            blocks = [...blocks, editableInput.slice(lastSplit)];
+        }
     }
 
-    return ['PART_2_SOLUTION'];
+    const maxBlockLength = Math.max(...blocks.map(block => block.length));
+    const blockLengthPaths = findTribonacci(maxBlockLength);
+
+    const blockPaths = blocks.map(block => blockLengthPaths[block.length - 1]);
+    
+    const finalNum = blockPaths.reduce((acc, val) => acc *= val, 1);
+
+    if (consoleOutput) {
+        console.log(`The answer to the part 2 is => ${finalNum}`); // Example: 19208, Normal: 99214346656768
+    }
+
+    return [finalNum];
 }
-solvePart2('INPUT');
+solvePart2(numRows);
+
+
+/*---------------*/
+/*-- Utilities --*/
+/*---------------*/
+function findTribonacci(num) {
+    let tribonacci = [1];
+
+    for (let i = 1; i < num; i++) {
+        const lastThree = tribonacci.slice(i - 3 > 0 ? i - 3 : 0, i);
+        const lastThreeSum = lastThree.reduce((acc, val) => acc += val);
+
+        tribonacci.push(lastThreeSum);
+    }
+
+    return tribonacci;
+}
